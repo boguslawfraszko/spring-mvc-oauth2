1c9c0c857c86ca592495fb4c3f4f6a435419acd0
Boguslaw Fraszko <boguslaw.fraszko@gmail.com>
2023-10-22 19:46:18 +0200

    use authorizedClientService

--- src/main/java/com/example/spring/security/springmvcoauth2/controller/OAuth2ClientController.java
+++ src/main/java/com/example/spring/security/springmvcoauth2/controller/OAuth2ClientController.java
@@ -1,8 +1,13 @@
 package com.example.spring.security.springmvcoauth2.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/oauth2")
+@Slf4j
 public class OAuth2ClientController {
     @Autowired
     private ClientRegistrationRepository clientRegistrationRepository;
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
 
     @GetMapping("/clients/{name}")
-    public ClientRegistration index(@PathVariable("name") String name) {
+    public ClientRegistration getClientRegistration(@PathVariable("name") String clientRegistrationId) {
         ClientRegistration clientRegistration =
-                this.clientRegistrationRepository.findByRegistrationId(name);
+                this.clientRegistrationRepository.findByRegistrationId(clientRegistrationId);
         return clientRegistration;
+    }
+
+    @GetMapping("/access-token/{name}")
+    public OAuth2AccessToken getAccessToken(Authentication authentication, @PathVariable("name") String clientRegistrationId) {
+        log.info("authentication "+ authentication);
+        OAuth2AuthorizedClient authorizedClient =
+                this.authorizedClientService.loadAuthorizedClient(clientRegistrationId, authentication.getName());
+
+        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+
+        return accessToken;
     }
 }
